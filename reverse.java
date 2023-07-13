2074a5adb1f70da3d62d4ad534eac0925ab08e43
AnanthkrishnanJM <138993678+AnanthkrishnanJM@users.noreply.github.com>
2023-07-13 15:07:43 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,17 @@
+package jmak;
+
+public class reverse {
+
+	public static void main(String[] args) {
+		
+		int a[]= {1,2,3,2,1};
+	
+		for(int i=4;i>=0;i--){
+			System.out.println(a[i]);
+			
+		}
+
+	}
+
+}
+  
